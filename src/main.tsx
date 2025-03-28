0eb2cb203a796aa4ee4301d60beb792497cbe131
CozyBrian <dragonbrian33@gmail.com>
2025-03-28 22:36:00 +0000

    chore: setup auth stuff

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,21 @@
-import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthProvider, { useAuth } from "./context/auth-provider";
+import "./index.css";
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
 
 // Create a new router instance
-const router = createRouter({ routeTree })
+const router = createRouter({ 
+  routeTree,
+  context: {
+    auth: undefined!,
+  },
+ })
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
@@ -14,14 +23,31 @@ declare module '@tanstack/react-router' {
     router: typeof router
   }
 }
+const queryClient = new QueryClient();
 
-// Render the app
-const rootElement = document.getElementById('root')!
+function InnerApp() {
+  const auth = useAuth();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} context={{ auth }} />
+    </QueryClientProvider>
+  );
+}
+
+function App() {
+  return (
+    <AuthProvider>
+      <InnerApp />
+    </AuthProvider>
+  );
+}
+
+const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement)
+  const root = ReactDOM.createRoot(rootElement);
   root.render(
-    <StrictMode>
-      <RouterProvider router={router} />
-    </StrictMode>,
-  )
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
 }
