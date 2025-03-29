a30807229c44aa3a859ea3274f720f4c8ef5c1d0
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2025-03-29 10:44:02 +0000

    move to tanstack start

---
+++ app/context/auth-provider.tsx
@@ -0,0 +1,53 @@
+import { wait } from "app/lib/utils";
+import React from "react";
+
+export interface user {
+  email: string;
+  token: string;
+}
+export interface IAuthContext {
+  isAuthenticated: boolean;
+  login: (data: user) => Promise<void>;
+  logout: () => Promise<void>;
+  user: user | null;
+}
+
+const AuthContext = React.createContext<IAuthContext | null>(null);
+
+const key = "syka.auth.user";
+
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = React.useState<user | null>(null);
+  const isAuthenticated = !!user;
+
+  const logout = React.useCallback(async () => {
+    await wait(250);
+
+    setUser(null);
+  }, []);
+
+  const login = React.useCallback(async (data: user) => {
+    await wait(500);
+
+    setUser(data);
+  }, []);
+
+  React.useEffect(() => {}, []);
+
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export function useAuth() {
+  const context = React.useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+}
+
+export default AuthProvider;
---
+++ app/routes/__root.tsx
@@ -0,0 +1,63 @@
+import AuthProvider from "@/context/auth-provider";
+import {
+  createRootRoute,
+  HeadContent,
+  Outlet,
+  Scripts,
+} from "@tanstack/react-router";
+import type { ReactNode } from "react";
+
+import appCss from "@/styles/app.css?url";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+      {
+        title: "Lumon",
+      },
+    ],
+    links: [
+      {
+        rel: "stylesheet",
+        href: appCss,
+      },
+    ],
+  }),
+  component: RootComponent,
+});
+
+function RootComponent() {
+  const queryClient = new QueryClient();
+
+  return (
+    <RootDocument>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <Outlet />
+        </AuthProvider>
+      </QueryClientProvider>
+    </RootDocument>
+  );
+}
+
+function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
+  return (
+    <html>
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        {children}
+        <Scripts />
+      </body>
+    </html>
+  );
+}
